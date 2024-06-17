2757626e99878cb7821db9d41541ad23c73dac77
Mauritz Koekemoer <mauritz@koekemoer.nz>
2024-06-17 16:32:14 +1200

    Added DB Interaction, User Model & Class + User Registration Functionality

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import { createApp } from 'vue';
+import { createApp } from 'vue';  // Import the createApp function from Vue
+
+// Import custom Vue components
 import NavMainHero from './nav_main_hero.vue';
 import NavOnly from './nav_only.vue';
-import UserRegistration from './registration_form.vue';
+import RegistrationForm from './registration_form.vue';
 import Footer from './footer.vue';
 
+// Create a Vue app instance with the component and mount it to the element with a matching ID
 createApp(NavMainHero).mount('#nav_main_hero');
 createApp(NavOnly).mount('#nav_only');
-createApp(UserRegistration).mount('#user_registration');
+createApp(RegistrationForm).mount('#user_registration');
 createApp(Footer).mount('#footer');
