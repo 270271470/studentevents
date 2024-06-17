b185a7efbcb8759d62e57fa5af2bf59480fe84c1
Mauritz Koekemoer <mauritz@koekemoer.nz>
2024-06-17 13:47:30 +1200

    Added registration form

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def welcome_home():
     return render_template("index.html")
 
 
+@app.route('/register')
+def register():
+    return render_template("user/register.html")
+
+
 if __name__ == '__main__':
     app.run()
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue';
 import NavMainHero from './nav_main_hero.vue';
+import NavOnly from './nav_only.vue';
+import UserRegistration from './registration_form.vue';
 import Footer from './footer.vue';
 
-createApp(NavMainHero).mount('#app');
+createApp(NavMainHero).mount('#nav_main_hero');
+createApp(NavOnly).mount('#nav_only');
+createApp(UserRegistration).mount('#user_registration');
 createApp(Footer).mount('#footer');
