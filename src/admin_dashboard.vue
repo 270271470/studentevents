50a6678af13b53bc4909a494d275119b02a225bd
Mauritz Koekemoer <mauritz@koekemoer.nz>
2024-06-21 19:19:38 +1200

    Font and layout changes to user and admin dashboards

--- src/admin_dashboard.vue
+++ src/admin_dashboard.vue
@@ -88,74 +88,42 @@
       </header>
 
 
-<div>
-  <h2 class="text-sm font-medium text-gray-500 mt-5 mb-5">Quick Links</h2>
+<div class="mb-10">
+  <h2 class="text-xl font-medium text-gray-500 mt-5 mb-5">Quick Links</h2>
   <ul role="list" class="mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4">
     <li class="col-span-1 flex rounded-md shadow-sm h-32">
       <div class="flex w-16 flex-shrink-0 items-center justify-center rounded-l-md bg-pink-600 text-sm font-medium text-white"></div>
       <div class="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
-        <div class="flex-1 truncate px-4 py-2 text-sm">
-          <a href="#" class="font-medium text-gray-900 hover:text-gray-600">Manage Users</a>
-          <p class="text-gray-500">16 Users</p>
-        </div>
-        <div class="flex-shrink-0 pr-2">
-          <button type="button" class="inline-flex h-8 w-8 items-center justify-center rounded-full bg-transparent bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-            <span class="sr-only">Open options</span>
-            <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-              <path d="M10 3a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM10 8.5a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM11.5 15.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0Z" />
-            </svg>
-          </button>
+        <div class="flex-1 truncate px-4 py-2">
+          <a href="#" class="font-medium text-gray-900 hover:text-gray-600 text-lg">Manage Users</a>
+          <p class="text-gray-500 text-sm">16 Users</p>
         </div>
       </div>
     </li>
     <li class="col-span-1 flex rounded-md shadow-sm">
       <div class="flex w-16 flex-shrink-0 items-center justify-center rounded-l-md bg-purple-600 text-sm font-medium text-white"></div>
       <div class="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
-        <div class="flex-1 truncate px-4 py-2 text-sm">
-          <a href="#" class="font-medium text-gray-900 hover:text-gray-600">Pending Bookings</a>
-          <p class="text-gray-500">12 Pending</p>
-        </div>
-        <div class="flex-shrink-0 pr-2">
-          <button type="button" class="inline-flex h-8 w-8 items-center justify-center rounded-full bg-transparent bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-            <span class="sr-only">Open options</span>
-            <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-              <path d="M10 3a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM10 8.5a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM11.5 15.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0Z" />
-            </svg>
-          </button>
+        <div class="flex-1 truncate px-4 py-2">
+          <a href="#" class="font-medium text-gray-900 hover:text-gray-600 text-lg">Pending Bookings</a>
+          <p class="text-gray-500 text-sm">12 Pending</p>
         </div>
       </div>
     </li>
     <li class="col-span-1 flex rounded-md shadow-sm">
       <div class="flex w-16 flex-shrink-0 items-center justify-center rounded-l-md bg-yellow-500 text-sm font-medium text-white"></div>
       <div class="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
-        <div class="flex-1 truncate px-4 py-2 text-sm">
-          <a href="#" class="font-medium text-gray-900 hover:text-gray-600">View All Bookings</a>
-          <p class="text-gray-500">16 Active Booking</p>
-        </div>
-        <div class="flex-shrink-0 pr-2">
-          <button type="button" class="inline-flex h-8 w-8 items-center justify-center rounded-full bg-transparent bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-            <span class="sr-only">Open options</span>
-            <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-              <path d="M10 3a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM10 8.5a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM11.5 15.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0Z" />
-            </svg>
-          </button>
+        <div class="flex-1 truncate px-4 py-2">
+          <a href="#" class="font-medium text-gray-900 hover:text-gray-600 text-lg">View All Bookings</a>
+          <p class="text-gray-500 text-sm">16 Active Booking</p>
         </div>
       </div>
     </li>
     <li class="col-span-1 flex rounded-md shadow-sm">
       <div class="flex w-16 flex-shrink-0 items-center justify-center rounded-l-md bg-green-500 text-sm font-medium text-white"></div>
       <div class="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
-        <div class="flex-1 truncate px-4 py-2 text-sm">
-          <a href="#" class="font-medium text-gray-900 hover:text-gray-600">Manage Events</a>
-          <p class="text-gray-500">8 Events</p>
-        </div>
-        <div class="flex-shrink-0 pr-2">
-          <button type="button" class="inline-flex h-8 w-8 items-center justify-center rounded-full bg-transparent bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-            <span class="sr-only">Open options</span>
-            <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-              <path d="M10 3a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM10 8.5a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM11.5 15.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0Z" />
-            </svg>
-          </button>
+        <div class="flex-1 truncate px-4 py-2">
+          <a href="#" class="font-medium text-gray-900 hover:text-gray-600 lg">Manage Events</a>
+          <p class="text-gray-500 text-sm">8 Events</p>
         </div>
       </div>
     </li>
