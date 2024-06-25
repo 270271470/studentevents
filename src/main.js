5bcdb1f7a619ec93291995e86f8f6977e58a9564
Mauritz Koekemoer <mauritz@koekemoer.nz>
2024-06-25 14:29:21 +1200

    UI Changes

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ import AdminManageUsers from './admin_manage_users.vue';
 import UserUpdateInfo from './user_update_info.vue';
 import UserLogin from './login.vue';
 import SupportCenter from './support-center.vue';
+import PrivacyPolicy from './privacy-policy.vue';
+import ServiceTerms from './terms-of-service.vue';
 import ContactUs from './contact-us.vue';
 import Footer from './footer.vue';
 
@@ -23,5 +25,7 @@ createApp(AdminManageUsers).mount('#admin-manage-users');
 createApp(UserUpdateInfo).mount('#user-update-info');
 createApp(UserLogin).mount('#user-login');
 createApp(SupportCenter).mount('#support-center');
+createApp(PrivacyPolicy).mount('#privacy-policy');
+createApp(ServiceTerms).mount('#terms-of-service');
 createApp(ContactUs).mount('#contact-us');
 createApp(Footer).mount('#footer');
